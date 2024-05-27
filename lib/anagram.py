2c5aa54e73ea2a804476e3914c30c8613aa12cd7
Albert Cheruiyot <bettalbert99@gmail.com>
2024-05-27 16:33:38 +0300

    First commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        for w in words:
+            # Convert both the current word and the target word to lowercase
+            lowercase_w = w.lower()
+            lowercase_self_word = self.word.lower()
+            
+            # Check if the current word is not identical to the target word, but they are anagrams
+            if lowercase_w != lowercase_self_word and sorted(lowercase_w) == sorted(lowercase_self_word):
+                # If they are anagrams, append the current word to the list of anagrams
+                anagrams.append(w)
+        
+        # Return the list of anagrams
+        return anagrams
+    pass
